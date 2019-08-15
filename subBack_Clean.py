039c4e708d13c1d65e33f154c0e5e03b46235ce6
Shourya Aggarwal <aggarwal.shourya@gmail.com>
2019-08-15 23:02:37 +0530

    Minor Changes

--- subBack_Clean.py
+++ subBack_Clean.py
@@ -1,8 +1,8 @@
 import numpy as np;
 import cv2
-import os
+import os, sys
 
-kernel = np.ones((5,5),np.uint8)
+kernel = np.ones((8,8),np.uint8)
 
 # Function to extract frames
 def FrameCapture(name):
@@ -23,18 +23,75 @@ def FrameCapture(name):
 	
 def BGSubtraction(name):
 	cap = cv2.VideoCapture("dataset/"+name+".mp4")
-	fgbg = cv2.BackgroundSubtractorMOG2(200,40,False)
+	fgbg = cv2.createBackgroundSubtractorMOG2(200,16,False);
 	while(1):
 		ret, frame = cap.read()
 		fgmask = fgbg.apply(frame)
 		opening = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-		cv2.imshow('frame',opening)
+		cv2.imwrite('temp.jpg',opening)
+		sobeled = applySobel('temp.jpg')
+		cv2.imshow('frame',sobeled)
 		k = cv2.waitKey(30) & 0xff
 		if k==27:
 			break;
 			
 	cap.release()
 	cv2.destroyAllWindows()
+	
+def applySobel(image_path):
+	scale = 1
+	delta = 0
+	ddepth = cv2.CV_16S	
+	src = cv2.imread(image_path, cv2.IMREAD_COLOR)
+	src = cv2.GaussianBlur(src, (3, 3), 0)
+	gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+	grad_x = cv2.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+	# Gradient-Y
+	grad_y = cv2.Scharr(gray,ddepth,0,1)
+	#grad_y = cv2.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+	abs_grad_x = cv2.convertScaleAbs(grad_x)
+	abs_grad_y = cv2.convertScaleAbs(grad_y)  
+	grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+	cv2.imwrite('temp.jpg',grad)
+	houghed = applyHough('temp.jpg')
+	return houghed
+	#cv.imshow(window_name, grad)
+	#cv.waitKey(0)
+	
+def applyHough(image):
+	img = cv2.imread(image)
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	edges = cv2.Canny(gray,50,150,apertureSize = 3)
+	lines = cv2.HoughLines(edges,1,np.pi/180,120)
+	if lines is not None:
+		number = 0;
+		x1_mean = y1_mean = x2_mean = y2_mean = 0;
+		
+		for line in lines:
+			rho,theta = line[0]
+			if rho < 0:
+				rho*=-1
+				theta-=np.pi
+			number = number + 1;
+			a = np.cos(theta)
+			b = np.sin(theta)
+			x0 = a*rho
+			y0 = b*rho
+			x1 = int(x0 + 1000*(-b))
+			y1 = int(y0 + 1000*(a))
+			x2 = int(x0 - 1000*(-b))
+			y2 = int(y0 - 1000*(a))
+			x1_mean = (x1_mean+x1)/number
+			y1_mean = (y1_mean+y1)/number
+			x2_mean = (x2_mean+x2)/number
+			y2_mean = (y2_mean+y2)/number
+			cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+		x1 = round(x1_mean);
+		x2 = round(x2_mean);
+		y1 = round(y1_mean);
+		y2 = round(y2_mean);
+	
+	return img
 
 
 with open('filelist.txt') as f:
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
 cap = cv2.VideoCapture('dataset/1.mp4')
-fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
+#fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 ret  = 1;
 k2 = np.ones((4,4),np.uint8)
 kernel = np.ones((8,8),np.uint8)
@@ -11,9 +12,9 @@ while(ret & cap.isOpened()):
     erosion = cv2.erode(fgmask,k2,iterations=1)
     opening = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, kernel)
     #erosion = cv2.erode(opening,k2,iterations = 1)
-    cv2.imshow('frame',fgmask)
+    #cv2.imshow('frame',fgmask)
     cv2.imshow('opened', opening)
-    cv2.imshow('eroded',erosion)
+    #cv2.imshow('eroded',erosion)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
